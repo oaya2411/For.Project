2e710dfb8a18cc3da1e2c097bd31bf47f3787542
Aya <oaya2411@gmail.com>
2025-04-10 09:33:20 +0200

    Signup for freelancer and client

---
+++ JS/prePage.js
@@ -0,0 +1,19 @@
+const radios = document.querySelectorAll('input[name="CorF"]');
+const submitBtn = document.getElementById("submitBtn");
+let selected; 
+radios.forEach(radio => {
+    radio.addEventListener("change", () => {
+        selected = document.querySelector('input[name="CorF"]:checked');
+        submitBtn.disabled = false;
+
+    });
+
+    document.getElementById("submitBtn").addEventListener("click", function () {
+        if (selected && selected.value === "freelancer") {
+        window.location.href = "freelancerRegister.html";
+        } else if (selected && selected.value === "client") {
+        window.location.href = "clientRegister.html";
+        }
+    });
+});
+
---
+++ JS/showpassword.js
@@ -0,0 +1,30 @@
+let eyeicons = document.getElementsByClassName("eyeicon");
+let passes = document.getElementsByClassName("pass");
+
+for (let i = 0; i < eyeicons.length; i++) {
+  eyeicons[i].onclick = function() {
+    if (passes[i].type === 'password') {
+      passes[i].type = 'text';
+      eyeicons[i].src = 'images/eye.png'
+    } else {
+      passes[i].type = 'password';
+      eyeicons[i].src = 'images/visibility.png'
+
+    }
+  }
+}
+
+
+fetch('https://restcountries.com/v3.1/all')
+            .then(response => response.json())
+            .then(countries => {
+                const countrySelect = document.getElementById("countries");
+                
+                countries.forEach(country => {
+                    const option = document.createElement("option");
+                    option.value = country.name.common; 
+                    option.text = country.name.common; 
+                    countrySelect.appendChild(option);
+                });
+            })
+            .catch(error => console.error('Error fetching countries:', error));
