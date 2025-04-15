d84ef083c4eea550259ed3fba8f7508b3bc7e848
Aya <oaya2411@gmail.com>
2025-04-16 01:40:45 +0200

    partOfResponsiveLandingPage

--- JS/showpassword.js
+++ JS/showpassword.js
@@ -19,11 +19,15 @@ fetch('https://restcountries.com/v3.1/all')
             .then(response => response.json())
             .then(countries => {
                 const countrySelect = document.getElementById("countries");
-                
                 countries.forEach(country => {
                     const option = document.createElement("option");
-                    option.value = country.name.common; 
-                    option.text = country.name.common; 
+                    if (country.name.common === "Israel") {
+                      option.value = "Palestien";
+                      option.text = "Palestien";
+                  } else {
+                      option.value = country.name.common;
+                      option.text = country.name.common;
+                  }
                     countrySelect.appendChild(option);
                 });
             })
